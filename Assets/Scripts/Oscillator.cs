ee9b5a326d11207d5291ce8fd4c21b8888ad11f7
Konrad <goostaw.projekt@gmail.com>
2022-07-09 14:46:37 +0200

    oscillator script finished

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float peroid = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
+        Debug.Log(startingPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float cycles = Time.time / peroid; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position =  startingPosition + offset;
     }
 }
